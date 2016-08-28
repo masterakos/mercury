9942c385ac7bf1714189a3aa332dfb0df122cc8e
Kostas Tsalikidis <tsalikidhs@gmail.com>
2016-08-29 00:57:57 +0300

    added ultronjs and updated tests

--- tests/test.js
+++ tests/test.js
@@ -1,35 +1,34 @@
-var Ultron = require('./ultron');
+var Ultron = require('ultronjs');
 var exec = require('child_process').exec;
 
 var ultron = new Ultron('chrome');
-
 var seedDatabase = new Promise(function(resolve, reject) {
   exec('cd ../ && php artisan db:seed && cd tests', function (error, stdout, stderr) { resolve(); });
 });
 
 var loginTest = ultron
   .it("should login")
-  .describe(function(open, wait, fill, click, $) {
-    open('http://localhost/mercury')
-    wait('.top-header .btn-login').toAppear();
-    click('.top-header .btn-login');
-    wait('#login-form').toAppear();
-    fill('#login-form .email-input').with('john_smith@gmail.com');
-    fill('#login-form .password-input').with('johnjohn');
-    click('#login-form .btn-login');
-    wait('#account-dropdown').toAppear();
-    $('#account-dropdown').should.haveContent('John');
+  .describe(function() {
+    this.open('http://localhost/mercury')
+    this.wait('.top-header .btn-login').toAppear();
+    this.click('.top-header .btn-login');
+    this.wait('#login-form').toAppear();
+    this.fill('#login-form .email-input').with('john_smith@gmail.com');
+    this.fill('#login-form .password-input').with('johnjohn');
+    this.click('#login-form .btn-login');
+    this.wait('#account-dropdown').toAppear();
+    this.$('#account-dropdown').should.haveContent('John');
   })
 
 var logoutTest = ultron
   .it("should logout")
   .describe(function(open, wait, fill, click, $) {
-    open('http://localhost/mercury')
-    wait('#account-dropdown').toAppear();
-    click('#account-dropdown');
-    click('.top-header .btn-logout');
-    wait('.top-header .btn-login').toAppear();
-    $('.top-header').should.haveContent('Login');
+    this.open('http://localhost/mercury')
+    this.wait('#account-dropdown').toAppear();
+    this.click('#account-dropdown');
+    this.click('.top-header .btn-logout');
+    this.wait('.top-header .btn-login').toAppear();
+    this.$('.top-header').should.haveContent('Login');
   })
   
 seedDatabase
