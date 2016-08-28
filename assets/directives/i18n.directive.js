37b3e5b8b5a56c3831da0d00d5a9cf147ae7140b
Kostas Tsalikidis <tsalikidhs@gmail.com>
2016-08-28 09:17:23 +0300

    updated i18n directive in order to handle undefined input

--- assets/directives/i18n.directive.js
+++ assets/directives/i18n.directive.js
@@ -1,15 +1,17 @@
 angular
   .module('MercuryApp')
-  .directive('i18n', ['$rootScope', 'localizationService', '$filter', function($rootScope, localizationService, $filter) {
+  .directive('i18n', ['$rootScope', 'LocalizationService', '$filter', function($rootScope, LocalizationService, $filter) {
     return function(scope, element, attrs) {
       function applyLocal() {
-        var translation = localizationService.language.translations[attrs.i18n];
-        if (attrs.filter) {
-          attrs.filter.split(' ').forEach(function(filter) {
-            translation = $filter(filter)(translation);
-          });
+        var translation = LocalizationService.language.translations[attrs.i18n];
+        if (angular.isDefined(translation)) {
+          if (attrs.filter) {
+            attrs.filter.split(' ').forEach(function(filter) {
+              translation = $filter(filter)(translation);
+            });
+          }
+          element.text(translation);  
         }
-        element.text(translation);  
       }
       
       applyLocal();
