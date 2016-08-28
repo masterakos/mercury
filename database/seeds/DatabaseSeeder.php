b104a18ea8d761312b1e01540f67ce79b53ee202
Kostas Tsalikidis <tsalikidhs@gmail.com>
2016-08-28 18:29:35 +0300

    added users seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,15 +2,14 @@
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // $this->call(UsersTableSeeder::class);
-    }
+class DatabaseSeeder extends Seeder {
+  public function run() {
+    DB::table('users')->insert([
+      'uuid' => '1337',
+      'email' => 'john_smith@gmail.com',
+      'password' => '$2y$10$NJmV560MKgpZOE/HriAgQO/0CHuZXHTg6ZOkiqwYwu7isTtKMORD2', // hashed 'johnjohn'
+      'firstname' => 'John',
+      'lastname' => 'Smith'
+    ]);
+  }
 }
