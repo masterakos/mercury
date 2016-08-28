5ac32dcc04203b78b1f59112c9aaa6e4cb91b1dc
Kostas Tsalikidis <tsalikidhs@gmail.com>
2016-08-28 09:19:57 +0300

    updated titlelize filter to handle empty input and keep spaces

--- assets/filters/titlelize.filter.js
+++ assets/filters/titlelize.filter.js
@@ -2,10 +2,11 @@ angular
   .module('MercuryApp')
   .filter('titlelize', function() {
     return function(text) {
-      var titlelized = '';
-      text.split(' ').forEach(function(word) {
-        titlelized += word[0].toUpperCase()  + word.substring(1);
-      });
-      return titlelized;
+      if (!text) return text;
+      return text.split(' ').map(function(word) {
+        return word.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").length > 3
+          ? word[0].toUpperCase() + word.substr(1)
+          : word;
+      }).join(' ');
     };
   });
